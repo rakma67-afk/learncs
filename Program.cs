8b89445ae006fc9fac481655e3fa516cd95a440c
rakma67-afk <rak.ma.67@ubu.ac.th>
2026-06-17 10:18:03 +0700

    update 000

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace learncs
         static void Main(string[] args)
         {
             Console.WriteLine("'Hello World!'");
+            /*
+             * int a = 10;
+             * float b = 20.5f;
+             * duble c = 30.5;
+             * char d = 'A';
+             * string e = "Hello";
+             * 
+             * var f = 100; // Implicitly typed variable
+             * const double pi = 3.14159; // Constant variable
+             */
+
+            string[] fruits = { "Apple", "Banana", "Orange" };
+
+            string[] cars = new string[3];
         }
     }
 }
