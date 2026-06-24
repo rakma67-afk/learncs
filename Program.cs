2264d9236074c054f73643fcf1a222e91eededaf
rakma67-afk <rak.ma.67@ubu.ac.th>
2026-06-24 09:56:17 +0700

    update 001

--- Program.cs
+++ Program.cs
@@ -72,6 +72,30 @@ namespace learncs
                 Console.WriteLine(car);
             }
             
+            string Hello(string name)
+            {
+                return $"Hello {name} from the Hello method!";
+            }
+
+            Console.WriteLine(Hello("Jay"));
+
+            string NumberCompare(int num1, int num2)
+            {
+                if (num1 > num2)
+                {
+                    return $"{num1} is greater than {num2}";
+                }
+                else if (num1 < num2)
+                {
+                    return $"{num1} is less than {num2}";
+                }
+                else
+                {
+                    return $"{num1} is equal to {num2}";
+                }
+            }
+
+            Console.WriteLine( NumberCompare(10, 20));
         }
     }
 }
